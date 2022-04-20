2593b2bcd29403d5e0cb932ddb516352a6dceb89
MarioAv <mario.azcvei@hotmail.com>
2022-04-20 16:31:53 +0200

    pages

--- app/Console/Commands/InstaladorCommand.php
+++ app/Console/Commands/InstaladorCommand.php
@@ -138,7 +138,7 @@ class InstaladorCommand extends Command
         }
 
         $this->info('Seeding data into the database');
-        $this->seed('VoyagerDatabaseSeeder');
+        $this->seed('DatabaseSeeder');
 
         if ($this->option('with-dummy')) {
             $this->info('Publishing dummy content');
