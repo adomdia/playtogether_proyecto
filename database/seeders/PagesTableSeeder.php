d9b74324db56f93eb0d5594a94752aa10064245e
MarioAv <mario.azcvei@hotmail.com>
2022-04-20 16:50:05 +0200

    pages

--- database/seeders/PagesTableSeeder.php
+++ database/seeders/PagesTableSeeder.php
@@ -97,7 +97,7 @@ class PagesTableSeeder extends Seeder
         $dataRow = $this->dataRow($pageDataType, 'body');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type'         => 'rich_text_box',
+                'type'         => 'contentbuilder',
                 'display_name' => __('voyager::seeders.data_rows.body'),
                 'required'     => 1,
                 'browse'       => 0,
