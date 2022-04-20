5e2a364214bfef08337dbd70cb86f97c58e633be
MarioAv <mario.azcvei@hotmail.com>
2022-04-20 16:32:33 +0200

    pages

--- app/Console/Commands/InstaladorCommand.php
+++ app/Console/Commands/InstaladorCommand.php
@@ -102,7 +102,7 @@ class InstaladorCommand extends Command
 
         $this->info('Migrating the database tables into your application');
 
-        $this->call('migrate', ['--force' => $this->option('force')]);
+        $this->call('migrate');
 
         $this->info('Attempting to set Voyager User model as parent to App\User');
         if (file_exists(app_path('User.php')) || file_exists(app_path('Models/User.php'))) {
