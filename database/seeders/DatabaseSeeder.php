b6f5c095a0dcbfb6b32f8e8c50653901bcaacbfa
MarioAv <mario.azcvei@hotmail.com>
2022-04-20 16:26:30 +0200

    pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->seed('DataTypesTableSeeder');
+        $this->seed('DataRowsTableSeeder');
+        $this->seed('MenusTableSeeder');
+        $this->seed('MenuItemsTableSeeder');
+        $this->seed('RolesTableSeeder');
+        $this->seed('PermissionsTableSeeder');
+        $this->seed('PermissionRoleTableSeeder');
+        $this->seed('SettingsTableSeeder');
     }
 }
