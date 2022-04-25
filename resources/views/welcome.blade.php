d5a33dac0e47166a40d3e7693ee22ef8c9992b5c
Mario Azcunaga <soporte12@xerintel.es>
2022-04-25 12:32:43 +0000

    welcome.blade.php editado a través de la web en Bitbucket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-testttt
+
 @section('content')
 
 <div
@@ -132,7 +132,7 @@ testttt
             </div>
 
             <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-               Versión Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+               Versiï¿½n Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
             </div>
         </div>
     </div>
